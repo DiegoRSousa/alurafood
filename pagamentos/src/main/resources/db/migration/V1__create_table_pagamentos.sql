03da864d7fdb12fcbad8adcc87a7dc7c72d18587
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-09-07 16:12:51 -0300

    ajusta script de pagamentos

--- pagamentos/src/main/resources/db/migration/V1__create_table_pagamentos.sql
+++ pagamentos/src/main/resources/db/migration/V1__create_table_pagamentos.sql
@@ -1,10 +1,10 @@
 CREATE TABLE pagamentos (
     id SERIAL primary key ,
     valor decimal(19,2) not null ,
-    nome varchar(100) default '',
-    numero varchar(19) default '',
+    nome varchar(100) not null,
+    numero varchar(19) not null,
     expiracao varchar(7) not null,
-    codigo varchar(3) default null,
+    codigo varchar(3) not null,
     status varchar(255) not null,
     forma_de_pagamento_id bigint not null,
     pedido_id bigint not null
