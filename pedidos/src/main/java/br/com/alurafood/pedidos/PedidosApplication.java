6cf32cf0ce1b614195bffab9b42de39a728e5828
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-11-04 09:13:44 -0300

    adiciona log na aplicacao

--- pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -1,5 +1,7 @@
 package br.com.alurafood.pedidos;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,8 +10,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class PedidosApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(PedidosApplication.class);
 	public static void main(String[] args) {
+		logger.info("Iniciando a aplicação de pedidos");
 		SpringApplication.run(PedidosApplication.class, args);
+		logger.info("Aplicação de cadastro de pedidos");
 	}
 
 }
